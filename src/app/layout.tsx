e94fbe8dbd067f28bdcae48d69a564910c94e91a
Oliver Rivett-Carnac <oliver@wootware.co.za>
2025-03-24 14:40:27 +0200

    Add theme to allow dark mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
 import { Header } from "@/components/header";
 import { Toaster } from "@/components/ui/sonner";
 
@@ -25,17 +26,22 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Header />
-        <main className="min-h-screen bg-background">
-          <div className="mx-auto max-w-7xl py-4">
-            {children}
-          </div>
-        </main>
-        <Toaster />
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Header />
+          <main className="min-h-screen bg-background">
+            <div className="mx-auto max-w-7xl py-4">{children}</div>
+          </main>
+          <Toaster />
+        </ThemeProvider>
       </body>
     </html>
   );
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,13 +1,15 @@
 import { Logo } from "./logo";
+import { ModeToggle } from "./mode-toggle";
 
 export function Header() {
   return (
     <header className="border-b">
-      <div className="mx-auto max-w-7xl flex h-12 items-centes">
+      <div className="mx-auto max-w-7xl flex h-12 items-center justify-between">
         <div className="flex items-center">
           <Logo />
           <h1 className="text-2xl font-bold">Spooly</h1>
         </div>
+        <ModeToggle />
       </div>
     </header>
   );
